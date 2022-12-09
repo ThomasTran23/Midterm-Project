7c9f2719677710314a4cad2cfcd38bb00ad4d28c
ThomasTran23 <nintommy@gmail.com>
2022-12-09 14:24:07 -0800

    12/9
    Co-authored-by: lucianfc21 <lucianfc21@users.noreply.github.com>

--- Fish.java
+++ Fish.java
@@ -1,3 +1,65 @@
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+import java.util.Random;
+import java.util.ArrayList;
+
 public class Fish {
+    private String name;
+    private int health;
+    private double attack;
+    private double speed;
+    private int level;
+
     
+    
+    //  new Fish("Carp", 30, 5, 10)
+
+
+    public Fish(String name, int health, double attack, double speed){
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+        this.speed = speed;
+    }
+
+    public String getName(){
+        return name;
+    }
+    
+    public int getHealth(){
+        return health;
+    }
+
+    public double getAttack(){
+        return attack;
+    }
+
+    public double getSpeed(){
+        return speed;
+    }
+
+    public int getLevel(){
+        return level;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void takeDamage(int damage){
+        this.health -= damage;
+    }
+
+    public void attackChange(int multiplier){
+        this.attack *= 1+ (multiplier/100);
+    }
+
+    public void speedChange(int multiplier){
+        this.speed *= 1 + (multiplier/100);
+    }
+
+    public void levelUp(int level){
+        this.level += level;
+    }
 }
